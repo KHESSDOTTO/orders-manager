f9c62644c1583980d35e697c1fea0ac9e2af8acc
Khess S Dotto <khessdotto@gmail.com>
2023-02-14 16:28:00 -0300

    Home done without proper styling - yet to make product buttons add productID to the URL (Paulo is going to add a productID key to the collection on the db first).

--- src/components/listOfProducts/index.jsx
+++ src/components/listOfProducts/index.jsx
@@ -1,3 +1,49 @@
-export function ListOfProducts() {
-  return <section></section>;
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { api } from "../../util/api";
+
+export function ListOfProducts(props) {
+  const [prodList, setProdList] = useState([]);
+
+  // Pesquisa toda a lista de produtos da api - collection "products" e salva a resposta em "response".
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await api.get("/products");
+        console.log(response);
+        setProdList(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  // Utiliza o texto passado como props ao componente (proveniente da searchBar) para filtrar a array de produtos e mostrar apenas os produtos com
+  // os nomes correspondentes.
+  return (
+    <section>
+      {prodList
+        .filter((cP) =>
+          cP.attributes.name
+            .toLowerCase()
+            .includes(props.textFilter.toLowerCase())
+        )
+        .map((currProduct) => {
+          return (
+            <article>
+              <img
+                src={currProduct.attributes.imageURL}
+                style={{ width: "15em" }}
+              />
+              <h2>{currProduct.attributes.name}</h2>
+              <p>{`Preço: R$ ${currProduct.attributes.price}`}</p>
+              <Link to="/">
+                <button>Acessar</button>
+              </Link>
+            </article>
+          );
+        })}
+    </section>
+  );
 }
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,11 +1,18 @@
 import { SearchBar } from "../../searchBar";
 import { ListOfProducts } from "../../listOfProducts";
+import { useState } from "react";
 
 export function Home() {
+  const [searchText, setSearchText] = useState("");
+
+  function search(text) {
+    setSearchText(text);
+  }
+
   return (
     <div>
-      <SearchBar />
-      <ListOfProducts />
+      <SearchBar filterProducts={search} />
+      <ListOfProducts textFilter={searchText} />
     </div>
   );
 }
