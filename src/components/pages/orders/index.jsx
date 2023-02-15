9b66ac3d082fb4b84b7f6e5b521af747630721e8
Khess S Dotto <khessdotto@gmail.com>
2023-02-15 10:01:57 -0300

    Added Orders page, component listOfOrders and route for it.

---
+++ src/components/pages/orders/index.jsx
@@ -0,0 +1,17 @@
+import { SearchBar } from "../../searchBar";
+import { ListOfOrders } from "../../listOfOrders";
+
+export function Orders() {
+  const [searchText, setSearchText] = useState("");
+
+  function search(text) {
+    setSearchText(text);
+  }
+
+  return (
+    <div>
+      <SearchBar filterProducts={search} />
+      <ListOfOrders textFilter={searchText} />
+    </div>
+  );
+}
