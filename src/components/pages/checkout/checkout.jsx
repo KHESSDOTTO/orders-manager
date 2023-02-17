f30b9190f483f5e011aa0e6a8d8be081b6714835
Paulo Kamibayashi <paulo_kamibayashi@hotmail.com>
2023-02-17 05:20:08 -0300

    cart style done

--- src/components/pages/checkout/checkout.jsx
+++ src/components/pages/checkout/checkout.jsx
@@ -1,12 +1,13 @@
-import ListGroup from "react-bootstrap/ListGroup";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { api } from "../../../util/api";
-import { v4 as uuid } from "uuid";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../../util/api';
+import { v4 as uuid } from 'uuid';
+import '../checkout/checkout.css';
 
 function Checkout(props) {
   const [cart, setCart] = useState(props.order);
-  const [shippingAddress, setShippingAddress] = useState("");
+  const [shippingAddress, setShippingAddress] = useState('');
   const navigate = useNavigate();
 
   console.log(cart);
@@ -37,11 +38,11 @@ function Checkout(props) {
           orderID: orderUUID,
         },
       };
-      await api.post("/orders", orderData);
+      await api.post('/orders', orderData);
       console.log(orderData);
       props.order.splice(0, props.order.length);
       setCart([]);
-      navigate("/orders");
+      navigate('/orders');
     } catch (err) {
       console.log(err);
     }
@@ -76,21 +77,44 @@ function Checkout(props) {
       </section>
       {cart.map((item, index) => {
         return (
-          <article style={{ border: "1px solid black" }}>
-            <p>{item.name}</p>
-            <p>{`Preço: R$ ${item.price}`}</p>
-            <div>
-              <button onClick={() => handleDecrement(index)}>-</button>
-              <p>{`Quantidade: ${item.qnty}`}</p>
-              <button onClick={() => handleIncrement(index)}>+</button>
+          <article>
+            <img src={item.imageURL} alt="" />
+            <div className="orderDetails">
+              <p>{item.name}</p>
+              <p>{`Preço: R$ ${item.price}`}</p>
+              <div>
+                <button
+                  className="qntyButton"
+                  onClick={() => handleDecrement(index)}
+                >
+                  -
+                </button>
+                <p>{`Quantidade: ${item.qnty}`}</p>
+                <button
+                  className="qntyButton"
+                  onClick={() => handleIncrement(index)}
+                >
+                  +
+                </button>
+              </div>
+
+              <button
+                class="noselect"
+                onClick={(event) => handleDelete(event, item.UUID, index)}
+              >
+                <span class="text">Delete</span>
+                <span class="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                  </svg>
+                </span>
+              </button>
             </div>
-            <img src={item.imageURL} alt="" style={{ width: "10em" }} />
-            <button
-              variant="secondary"
-              onClick={(event) => handleDelete(event, item.UUID, index)}
-            >
-              Delete
-            </button>
           </article>
         );
       })}
